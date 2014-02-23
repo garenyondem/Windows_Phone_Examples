70248b2b8e933378a418ca566f0c1077f59002d4
Garen Yöndem <garen@yondem.com>
2014-02-23 17:25:23 +0200

    Example Device Status

---
+++ DeviceStatus/ExampleDeviceStatus/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Info;
+
+namespace ExampleDeviceStatus
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            brand.Text = DeviceStatus.DeviceManufacturer;
+            model.Text = DeviceStatus.DeviceName;
+            hardwareversion.Text = DeviceStatus.DeviceHardwareVersion;
+            devicetotalmemory.Text = DeviceStatus.DeviceTotalMemory.ToString();
+            currentmemory.Text = DeviceStatus.ApplicationCurrentMemoryUsage.ToString();
+            peakmemoryusage.Text = DeviceStatus.ApplicationPeakMemoryUsage.ToString();
+            firmware.Text = DeviceStatus.DeviceFirmwareVersion;
+            power.Text = DeviceStatus.PowerSource.ToString();
+
+            //DeviceStatus.IsKeyboardDeployed.ToString();
+            //DeviceStatus.IsKeyboardPresent.ToString();
+        }
+    }
+}
