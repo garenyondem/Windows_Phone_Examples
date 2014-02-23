878ecdd98774f08f75df0b750c91584f47f97160
Garen Yöndem <garen@yondem.com>
2014-02-23 17:26:58 +0200

    Example Panorama Selection Change Track

---
+++ PanoramaSelectionCanged/panorama selection changed/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using panorama_selection_changed.Resources;
+
+namespace panorama_selection_changed
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
---
+++ PanoramaSelectionCanged/panorama selection changed/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace panorama_selection_changed
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the data context of the listbox control to the sample data
+            DataContext = App.ViewModel;
+        }
+
+        private void Panorama_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (((Panorama)sender).SelectedIndex)
+            {
+                case 0: 
+                    ApplicationBar.IsVisible = true; 
+                    break;
+                case 1:
+                    ApplicationBar.IsVisible = false;
+                    break;
+                case 2:
+                    ApplicationBar.IsVisible = true;
+                    break;
+            }
+        }
+
+        // Load data for the ViewModel Items
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (!App.ViewModel.IsDataLoaded)
+            {
+                App.ViewModel.LoadData();
+            }
+        }
+    }
+}
